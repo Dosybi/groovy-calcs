f43c80cad666472241dcd469dc15671eecc58130
Dosybi <dosybi@gmail.com>
2022-12-23 16:11:33 +0600

    fix button link

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,17 @@
+import Link from 'next/link'
+
 const Button = ({ link, label, color }) => {
   return (
-    <button
-      className={`rounded-md border-2 border-gray-600 bg-white px-4 py-2 text-sm font-bold transition hover:border-gray-100 hover:bg-gradient-to-r ${
-        color.split(' ')[1]
-      } ${color.split(' ')[2]}`}
-      href={link}
-    >
-      {label}
-    </button>
+    <Link href={link}>
+      <button
+        className={`rounded-md border-2 border-gray-600 bg-white px-4 py-2 text-sm font-bold transition hover:border-gray-100 hover:bg-gradient-to-r ${
+          color.split(' ')[1]
+        } ${color.split(' ')[2]}`}
+        href={link}
+      >
+        {label}
+      </button>
+    </Link>
   )
 }
 
--- components/Card.js
+++ components/Card.js
@@ -5,7 +5,7 @@ import Icon from './Icon'
 
 const Card = ({ name, description, link, icon, color }) => {
   return (
-    <div className="mb-8 rounded-xl bg-gray-100 px-5 pt-5 pb-4 lg:mr-8 lg:flex lg:h-auto lg:w-[45%] lg:flex-col">
+    <div className="mb-8 rounded-xl bg-gray-100 px-5 pt-5 pb-6 lg:mr-8 lg:flex lg:h-auto lg:w-[45%] lg:flex-col">
       <div className="mb-4 flex lg:flex-col">
         <Link href={link}>
           <Icon icon={icon} alt={name} color={color} />
@@ -14,11 +14,11 @@ const Card = ({ name, description, link, icon, color }) => {
           <div className="mb-3 text-xl font-bold lg:mt-4 lg:text-xl">
             {name}
           </div>
-          <div className="mb-4">{description}</div>
-          <div className="">
-            <Button link={link} label="Открыть" color={color} />
-          </div>
         </div>
+      </div>
+      <div className="mb-4">{description}</div>
+      <div className="">
+        <Button link={link} label="Открыть" color={color} />
       </div>
     </div>
   )
