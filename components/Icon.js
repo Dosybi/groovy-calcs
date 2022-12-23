994587a7188369160b35702de78b6c2ace9a3ab2
Dosybi <dosybi@gmail.com>
2022-12-23 16:18:12 +0600

    fix icons size and layout

--- components/Icon.js
+++ components/Icon.js
@@ -2,7 +2,9 @@ import Image from 'next/image'
 
 const Icon = ({ icon, alt, color }) => {
   return (
-    <div className={`mr-8 rounded-3xl p-6 ${color} w-40`}>
+    <div
+      className={`mr-8 rounded-3xl p-6 ${color} flex h-40 w-40 justify-center`}
+    >
       <Image src={icon} width={100} height={100} alt={alt}></Image>
     </div>
   )
