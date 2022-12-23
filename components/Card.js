8983e8e349f6ad52e10ab6737eb1d35ae9c8cc23
Dosybi <dosybi@gmail.com>
2022-12-23 21:06:04 +0600

    fix card padding

--- components/Card.js
+++ components/Card.js
@@ -5,7 +5,7 @@ import Icon from './Icon'
 
 const Card = ({ name, description, link, icon, color }) => {
   return (
-    <div className="mb-8 rounded-xl bg-gray-100 px-5 pt-5 pb-6 lg:mr-8 lg:flex lg:h-auto lg:w-[45%] lg:flex-col">
+    <div className="mb-8 rounded-xl bg-gray-100 px-5 pt-7 pb-6 lg:mr-8 lg:flex lg:h-auto lg:w-[45%] lg:flex-col">
       <div className="mb-4 flex lg:flex-col">
         <Link href={link}>
           <Icon icon={icon} alt={name} color={color} />
