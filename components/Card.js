fcdb5bc9e0ea31cc2e92d90f5ad69dfc8ae62f61
Dosybi <dosybi@gmail.com>
2022-12-23 16:08:58 +0600

    fix card layout

--- components/Card.js
+++ components/Card.js
@@ -5,16 +5,20 @@ import Icon from './Icon'
 
 const Card = ({ name, description, link, icon, color }) => {
   return (
-    <div className="mb-8 rounded-xl bg-gray-100 px-5 pt-5 pb-6 lg:mr-8 lg:flex lg:h-auto lg:w-[45%] lg:flex-col">
+    <div className="mb-8 rounded-xl bg-gray-100 px-5 pt-5 pb-4 lg:mr-8 lg:flex lg:h-auto lg:w-[45%] lg:flex-col">
       <div className="mb-4 flex lg:flex-col">
         <Link href={link}>
           <Icon icon={icon} alt={name} color={color} />
         </Link>
-        <div className="text-2xl font-bold lg:mt-4 lg:text-xl">{name}</div>
-      </div>
-      <div className="mb-4">{description}</div>
-      <div className="">
-        <Button link={link} label="Открыть" color={color} />
+        <div>
+          <div className="mb-3 text-xl font-bold lg:mt-4 lg:text-xl">
+            {name}
+          </div>
+          <div className="mb-4">{description}</div>
+          <div className="">
+            <Button link={link} label="Открыть" color={color} />
+          </div>
+        </div>
       </div>
     </div>
   )
