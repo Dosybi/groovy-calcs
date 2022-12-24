b3474f03ad3605fc9be771420e5f616bfa5b721c
Dosybi <dosybi@gmail.com>
2022-12-24 13:47:46 +0600

    add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,33 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/public/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/public/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/public/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/public/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/public/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <link rel="shortcut icon" href="/public/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/public/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
