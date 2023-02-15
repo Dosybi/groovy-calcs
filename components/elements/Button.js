3a3a738e8dd7db545ec10a3999cb583e79f66aca
Dosybi <dosybi@gmail.com>
2023-02-15 11:22:18 +0600

    fix button hover color

--- components/elements/Button.js
+++ components/elements/Button.js
@@ -6,9 +6,8 @@ const Button = ({ link, label, color }) => {
     <Link href={link}>
       <button
         className={classNames(
-          'rounded-md border-2 border-gray-600 bg-white px-4 py-2 text-sm font-bold transition hover:border-gray-100 hover:bg-gradient-to-r',
-          color.split(' ')[1],
-          color.split(' ')[2]
+          'rounded-md border-2 border-gray-600 bg-white px-4 py-2 text-sm font-bold transition hover:border-gray-100',
+          `${color}-button`
         )}
         href={link}
       >
