0bd0ff02fc0bb14ea34bd7004501b43e4b340104
Dosybi <dosybi@gmail.com>
2023-02-24 13:24:54 +0600

    refactor Button component

--- components/elements/Button.js
+++ components/elements/Button.js
@@ -1,28 +1,17 @@
 import Link from 'next/link'
-import classNames from 'classnames'
 
 const Button = ({ link, label, color, handleClick, isAdditional = false }) => {
-  if (isAdditional) {
-    return (
-      <button
-        className="mb-2 text-blue-600 hover:text-blue-800"
-        onClick={handleClick}
-      >
-        {label}
-      </button>
-    )
-  }
+  const className = isAdditional
+    ? 'mb-2 text-blue-600 hover:text-blue-800'
+    : `rounded-md border-2 border-gray-600 bg-white px-4 py-2 text-sm font-bold transition hover:border-gray-100 ${color}-button`
 
-  return (
-    <Link href={link}>
-      <button
-        className={classNames(
-          'rounded-md border-2 border-gray-600 bg-white px-4 py-2 text-sm font-bold transition hover:border-gray-100',
-          `${color}-button`
-        )}
-      >
-        {label}
-      </button>
+  return handleClick ? (
+    <button className={className} onClick={handleClick}>
+      {label}
+    </button>
+  ) : (
+    <Link href={link} passHref>
+      <button className={className}>{label}</button>
     </Link>
   )
 }
